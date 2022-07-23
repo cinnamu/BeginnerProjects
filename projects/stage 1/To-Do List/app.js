d62fd70cf7281078d201ff344f5d9a09e552a170
Adrian <88271962+cinnamu@users.noreply.github.com>
2022-07-23 10:42:06 -0400

    Add files via upload

---
+++ projects/stage 1/To-Do List/app.js
@@ -0,0 +1,12 @@
+//Set any variables
+
+const addList = document.getElementById('listAdd');
+const removeList = document.getElementById('listRemove');
+//Add to list functionality, link the input and the button
+addList.addEventListener("click", function(){
+    //const list = document.getElementsByTagName('ul');
+    //const input = document.getElementById('input');
+
+    document.getElementsByTagName('ul') + document.getElementById('input');
+})
+//Remove from list functionality
