2c5e810e2c2999edab36beed057aa41c76b12b14
Adrian <adrianralston@yahoo.com>
2022-07-27 15:49:26 -0400

    edits

--- stage 1/Vowel Counter/app.js
+++ stage 1/Vowel Counter/app.js
@@ -2,5 +2,9 @@ const btn = document.getElementById('btn');
 const input = document.getElementById('input');
 
 btn.addEventListener('click', function() {
+    const input = document.getElementById('input');
 
+
+
+    input.value = '';
 });
